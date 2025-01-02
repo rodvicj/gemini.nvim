6de976efce17b0eac9b6af10161263adcbf994a2
rodvicj <rodvicj@gmail.com>
2025-01-02 15:19:44 +0800

    update config

--- lua/gemini/config.lua
+++ lua/gemini/config.lua
@@ -4,11 +4,11 @@ local util = require('gemini.util')
 local M = {}
 
 local default_model_config = {
-  completion_delay = 600,
+  completion_delay = 500,
   model_id = api.MODELS.GEMINI_1_5_PRO,
-  temperature = 0.01,
-  top_k = 1.0,
-  max_output_tokens = 8196,
+  temperature = 1,
+  top_k = 40,
+  max_output_tokens = 8192,
   response_mime_type = 'text/plain',
 }
 
